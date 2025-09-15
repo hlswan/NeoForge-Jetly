9eac11cc186a154a5dad268d98d1048e4e2d8184
Henry Swanson <henry.swanson.usa@gmail.com>
2025-09-15 10:07:40 -0700

    Maple Syrup added

--- src/main/java/com/hlswan/jetly/item/ModItems.java
+++ src/main/java/com/hlswan/jetly/item/ModItems.java
@@ -2,13 +2,16 @@ package com.hlswan.jetly.item;
 
 import com.hlswan.jetly.Jetly;
 import com.hlswan.jetly.block.ModBlocks;
+import net.minecraft.client.gui.font.glyphs.BakedGlyph;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemUseAnimation;
+import net.minecraft.world.item.*;
 import net.minecraft.world.item.component.Consumable;
+import net.minecraft.world.item.consume_effects.ApplyStatusEffectsConsumeEffect;
+import net.minecraft.world.item.consume_effects.ConsumeEffect;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.bus.api.IEventBus;
@@ -31,6 +34,24 @@ public class ModItems {
                             .saturationModifier(0.1f)
                             .build()
             )
+    );
+
+    public static final DeferredItem<Item> MAPLE_SYRUP = ITEMS.registerSimpleItem(
+            "maple_syrup",
+            new Item.Properties().food(
+                    new FoodProperties.Builder()
+                            .nutrition(3)
+                            .saturationModifier(0.3f)
+                            .build()
+            )
+                    .usingConvertsTo(Items.GLASS_BOTTLE)
+                    .component(
+                            DataComponents.CONSUMABLE,
+                            Consumable.builder()
+                                    .consumeSeconds(2)
+                                    .sound(SoundEvents.HONEY_DRINK)
+                                    .build()
+                    )
     );
 
     public static final DeferredItem<BlockItem> ASH_LOG_ITEM =
